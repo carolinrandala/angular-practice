116daa04386bc5e345d2dc377ecda7eff2465e60
carolin randala <carolinrandala@gmail.com>
2023-02-14 20:57:26 +0200

    Learning modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,5 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppComponent } from './app.component';
 import { DogComponent } from './dog/dog.component';
 import { DogFormComponent } from './dog-form/dog-form.component';
@@ -8,6 +7,7 @@ import {FormsModule} from "@angular/forms";
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatTableModule} from "@angular/material/table";
 import {MatButtonModule} from "@angular/material/button";
+import {DashboardModule} from "./dashboard/dashboard.module";
 
 @NgModule({
   declarations: [
@@ -20,7 +20,8 @@ import {MatButtonModule} from "@angular/material/button";
     FormsModule,
     BrowserAnimationsModule,
     MatTableModule,
-    MatButtonModule
+    MatButtonModule,
+    DashboardModule
   ],
   providers: [],
   bootstrap: [AppComponent]
